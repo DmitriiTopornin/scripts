422dfdf138af10a1f1fe898df6d8fc0c3c2f7147
dtopornin <dtopornin@gmail.com>
2016-02-15 19:04:25 +0300

    rspec movie tests fix

--- spec/lib/movie_spec.rb
+++ spec/lib/movie_spec.rb
@@ -2,9 +2,19 @@ require "spec_helper"
 require "movie"
 
 describe Movie do 
+  
+  let (:movie){
+    {
+          "url":"http://www.imdb.com/title/tt0061722/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=2398042102&pf_rd_r=01JPHZE595567D9ZVSJ1&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_250",
+          "rating":"8.0","title":"The Graduate (1967)","year":"1967",
+          "duration":"106","genre":["Comedy","Drama","Romance"],
+          "country":"USA","date":"1967-12-22","director":"Mike Nichols",
+          "actors":"Dustin Hoffman, Anne Bancroft, Katharine Ross"
+        }
+  }
 
   let(:movies){
-      [
+      MyMoviesList.new([
         {
           "url":"http://www.imdb.com/title/tt0061722/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=2398042102&pf_rd_r=01JPHZE595567D9ZVSJ1&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_250",
           "rating":"8.0","title":"The Graduate (1967)","year":"1967",
@@ -26,14 +36,14 @@ describe Movie do
           "country":"Russia","date":"2002","director":"Fernando Meirelles,",
           "actors":"Alexandre Rodrigues, Matheus Nachtergaele, Leandro Firmino"
         }
-      ]
+      ])
     }
 
   context 'Movie initialize' do
 
     context 'parsing params' do
       context 'with date: %Y-%m or %Y-%m-%d' do
-        subject{Movie.category(movies[0], movies)}
+        subject{Movie.category(movie, movies)}
         it{should have_attributes(
           url: "http://www.imdb.com/title/tt0061722/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=2398042102&pf_rd_r=01JPHZE595567D9ZVSJ1&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_250",
           rating: "8.0",
@@ -49,7 +59,7 @@ describe Movie do
       end
 
       context 'with date: %Y' do
-        subject{Movie.category(movies[2], movies)}
+        subject{Movie.category(movie.merge(date: "2002"), movies)}
         it{should have_attributes(
           date: Date.strptime("2002", "%Y"),
         )}
@@ -60,28 +70,28 @@ describe Movie do
   context 'Movie object method' do
     context '#description' do
       it 'should return string' do
-        expect{subject(Movie.category(movies[0], movies).description).to eq("классический фильм, режиссер  %{director}, кол-во фильмов: %{director_movies_count}")}
+        expect{subject(Movie.category(movie, movies).description).to eq("классический фильм, режиссер  %{director}, кол-во фильмов: %{director_movies_count}")}
       end
     end
 
     context '#to_h' do
-      it 'should return hash' do
-        expect{subject(Movie.category(movies[0], movies)).to_h
+      it 'should returb hash' do
+        expect{subject(Movie.category(movie, movies)).to_h
           .to eq({year: "1967", director: "Mike Nichols", director_movies_count: 0, actors: "Dustin Hoffman, Anne Bancroft, Katharine Ross"})}
       end
     end
 
     context '#method_missing' do
       it 'should return true' do
-        expect{subject(Movie.category(movies[0], movies)).method_missing(:comedy).to be true}
+        expect{subject(Movie.category(movie, movies)).method_missing(:comedy).to be true}
       end
 
       it 'should raise error' do
-        expect{subject(Movie.category(movies[0], movies)).method_missing(:asd).to raise_error}
+        expect{subject(Movie.category(movie, movies)).method_missing(:asd).to raise_error}
       end
 
       it 'should raise ArgumentError' do
-        expect{subject(Movie.category(movies[0], movies)).method_missing.to raise_error(ArgumentError)}
+        expect{subject(Movie.category(movie, movies)).method_missing.to raise_error(ArgumentError)}
       end
     end
   end
