d4101092be7b7c65db332d7a1c1c1d874d1b3726
dtopornin <dtopornin@gmail.com>
2016-03-09 00:11:32 +0300

    fixes

--- spec/lib/tmdb_api_spec.rb
+++ spec/lib/tmdb_api_spec.rb
@@ -6,30 +6,36 @@ require 'tmdb_api.rb'
 WebMock.allow_net_connect!
 
 RSpec.describe 'tmdb api', vcr: true do
-  let(:tmdb_api) {TmdbApi.new.fetcher}
+  let(:tmdb_api) {
+    VCR.use_cassette('tmdb-fetch'){
+      TmdbApi.new.fetcher
+    }
+  }
 
 	describe 'TmdbApi all' do
 
-		subject {tmdb_api}
+    subject {tmdb_api}
 
-	  context 'should be an array' do
-		  it{should be_an Array}
-	  end
+    context 'should be an array' do
+      it{should be_an Array}
+    end
 
-	  context 'should contain 250 elements' do
-		  its(:count){should eq(250)}
-	  end
+    context 'should contain 250 elements' do
+      its(:count){should eq(250)}
+    end
 
-	  context 'element-hash should has genres' do
-		  it{should all be_a Hash}
-	  end
+    context 'element-hash should has genres' do
+      it{should all be_a Hash}
+    end
 
-	  context 'one element' do
-	  	subject {tmdb_api.first}
-      it{should have_key([:url,:rating,:title,:year,:duration,:genre,:country,:date,:director,:actors])}
+    context 'one element' do
+      subject {tmdb_api.first}
+      it 'should contain certain keys' do
+        expect(subject.keys).to contain_exactly(:url,:rating,:title,:year,:duration,:genre,:country,:date,:director,:actors)
+      end
       it 'should have genres' do
-      	expect(subject[:genre]).to all be_a String
+        expect(subject[:genre]).to all be_a String
       end
     end
-	end
+  end
 end
